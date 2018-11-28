affc3d4e06f9350940b042bc48685218470d95d0
shiyizhang93 <38091452+shiyizhang93@users.noreply.github.com>
2018-11-28 00:47:23 -0800

    make change on split_leaf and make_node

--- decisiontree.py
+++ decisiontree.py
@@ -5,6 +5,14 @@ import csv
 import sys, os, pdb
 from process_data import *
 
+class Node():
+  def__init__(self):
+    self.left_value = None
+    self.right_value = None
+    self.left_feture = None
+    self.right_feature = None
+    self.threshold = None
+    
 class DecisionTree():
 	def __init__(self, fn_train, fn_valid, fn_test=None):
 		self.csv_train = CSV(fn_train)
@@ -25,8 +33,8 @@ class DecisionTree():
       
   #Calculate gini index and the benefit of splite feature    
   def gini_benefit(self, cp, cm, clp, clm, crp, crm):
-    if clp+clm==0 or crp+crm==0:
-      return 0
+    #if clp+clm==0 or crp+crm==0:
+     # return 0
     pUl = (2*clp*clm/((clp+clm)*(cp+cm)))
     pUr = (2*crp*crm/((crp+crm)*(cp+cm)))
     Ua =  (2*cp*cm/(cp+cm)**2)
@@ -34,44 +42,57 @@ class DecisionTree():
   
   #split to different leaf
   def split_leaf(self, root_node, root_feature, threshold, para, leaf_clp, leaf_clm, leaf_crp, leaf_crm):
-    left_leaf = np.array([])
-    right_leaf = np.array([])
+    left_leaf_value = np.array([])
+    right_leaf_value = np.arry([])
+    left_leaf_feature = np.array([])
+    right_leaf_feature = np.array([])
+    
     for i in range(0,len(root_node[:])-1):
       y_value = root_node[i]
       if root_feature[i][para] <= T:
-        left_leaf = np.append(root_feature[i][para])
+        for j in range(0,len(root_feature[i][:])-1):
+          left_leaf_feature = np.append(root_feature[i][j])
+        left_leaf_value = np.append(root_node[i])
         if y_value == 1:
           leaf_clp += 1
         else:
           leaf_clm += 1
       else:
-        right_leaf = np.append(root_feature[i][para])
+        for k in range(0,len(root_feature[i][:])):
+          right_leaf_feature = np.append(root_feature[i][k])
+        right_leaf_value = np.append(root_node[i])
         if y_value == 1:
           leaf_crp += 1
         else:
           leaf_crm += 1
-  return left_leaf, right_leaf, leaf_clp, leaf_clm, leaf_crp, leaf_crm
+  return left_leaf_feature, right_leaf_feature, left_leaf_value, right_leaf_value, leaf_clp, leaf_clm, leaf_crp, leaf_crm
   
-  #Make a single node
+  #Make a single node with left and right
   def make_node(self, root_node, root_feature):
     #initialize the parameters
     #gini
     gini = 0
     gini_f = 0
-    gini_temp
+    gini_temp = 0
     #Threshold    
     T = 0
     T_f = 0
-    T_temp
-    #node array
-    left_node = np.array([])
-    right_node = np.array([])
-    left_node_f = np.array([])
-    right_node_f = np.array([])
-    left_node_temp = np.array([])
-    right_node_temp = np.array([])
+    T_temp = 0
+    #node feature
+    left_feature = np.matrix([])
+    right_feature = np.matrix([])
+    left_feature_f = np.matrix([])
+    right_feature_f = np.matrix([])
+    left_feature_temp = np.matrix([])
+    right_feature_temp = np.matrix([])
+    #node value
+    left_value = np.array([])
+    right_value = np.array([])
+    left_value_f = np.array([])
+    right_value_f = np.array([])
+    left_value_temp = np.array([])
+    right_value_temp = np.array([])
     #others
-    feature = 0
     tree_clp, tree_clm, tree_crp, tree_crm = 0, 0, 0, 0
     
     #Calculate the number of root node for result 3 and 5
@@ -81,23 +102,27 @@ class DecisionTree():
         temp += 1
     cp = temp
     cm = len(root_node[:])-temp
+    #Justify to split or not
+    if cp == 0 or cm == 0:
+      return 0
+    else:
     #Calculate gini-index and benefit
-    for j in range(0, len(root_feature[0][:])-1):
-      for k in range(0, len(root_feature[:][0])-1):
-        T_temp = self.x_train[k][j]
-        left_node_temp, right_node_temp, tree_clp, tree_clm, tree_crp, tree_crm = split_leaf(root_node, T, j, tree_clp, tree_clm, tree_crp, tree_crm)
-        gini_temp = gini_benefit(cp, cm, tree_clp, tree_clm, tree_crp, tree_crm)
-        if gini_temp > gini_f:
-          gini_f= gini_temp
-          left_node_f = left_node_temp
-          right_node_f = left_node_temp
-          T_f = T_temp
-      if gini_f > gini:
-        gini = gini_f
-        left_node = left_node_f
-        right_node = right_node_f
-        T = T_f
-  return gini, left_node, right_node, T
+      for j in range(0, len(root_feature[0][:])-1):
+        for k in range(0, len(root_feature[:][0])-1):
+          T_temp = self.x_train[k][j]
+          left_feature_temp, right_feature_temp, left_value_temp, right_value_temp, tree_clp, tree_clm, tree_crp, tree_crm = split_leaf(self, root_node, root_feature, T_temp, j, tree_clp, tree_clm, tree_crp, tree_crm)
+          gini_temp = gini_benefit(self, cp, cm, tree_clp, tree_clm, tree_crp, tree_crm)
+          if gini_temp > gini_f:
+            gini_f= gini_temp
+            left_node_f = left_node_temp
+            right_node_f = left_node_temp
+            T_f = T_temp
+        if gini_f > gini:
+          gini = gini_f
+          left_node = left_node_f
+          right_node = right_node_f
+          T = T_f
+    return left_feature, right_feature, left_value, right_value, T
   
   #Make a decision tree
   def make_tree()  
